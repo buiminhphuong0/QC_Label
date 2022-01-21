a3bb41bb5d40e480c3391c768970201895521107
MinhPhuong-IT <MinhPhuong-IT@MINHPHUONG>
2022-01-21 11:01:02 +0700

    Add project files.

---
+++ QC_Label/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace QC_Label
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbSup = new System.Windows.Forms.TextBox();
+            this.tbMaterial = new System.Windows.Forms.TextBox();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.tbSerial = new System.Windows.Forms.TextBox();
+            this.tbQuantity = new System.Windows.Forms.TextBox();
+            this.tbCavity = new System.Windows.Forms.TextBox();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.tbMate = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Supplier Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Material Code:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Date:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(20, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Serial Number:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(20, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Quantity:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(20, 260);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 24);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Cavity:";
+            // 
+            // tbSup
+            // 
+            this.tbSup.Enabled = false;
+            this.tbSup.Location = new System.Drawing.Point(201, 25);
+            this.tbSup.Name = "tbSup";
+            this.tbSup.Size = new System.Drawing.Size(200, 20);
+            this.tbSup.TabIndex = 6;
+            this.tbSup.Text = "SXDJ02";
+            // 
+            // tbMaterial
+            // 
+            this.tbMaterial.Enabled = false;
+            this.tbMaterial.Location = new System.Drawing.Point(201, 105);
+            this.tbMaterial.Name = "tbMaterial";
+            this.tbMaterial.Size = new System.Drawing.Size(200, 20);
+            this.tbMaterial.TabIndex = 8;
+            this.tbMaterial.Text = "620100000341";
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(201, 144);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpDate.TabIndex = 9;
+            // 
+            // tbSerial
+            // 
+            this.tbSerial.Location = new System.Drawing.Point(201, 185);
+            this.tbSerial.Name = "tbSerial";
+            this.tbSerial.Size = new System.Drawing.Size(130, 20);
+            this.tbSerial.TabIndex = 10;
+            // 
+            // tbQuantity
+            // 
+            this.tbQuantity.Location = new System.Drawing.Point(201, 225);
+            this.tbQuantity.Name = "tbQuantity";
+            this.tbQuantity.Size = new System.Drawing.Size(130, 20);
+            this.tbQuantity.TabIndex = 11;
+            // 
+            // tbCavity
+            // 
+            this.tbCavity.Location = new System.Drawing.Point(201, 265);
+            this.tbCavity.Name = "tbCavity";
+            this.tbCavity.Size = new System.Drawing.Size(200, 20);
+            this.tbCavity.TabIndex = 12;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrint.Location = new System.Drawing.Point(301, 302);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(100, 50);
+            this.btnPrint.TabIndex = 13;
+            this.btnPrint.Text = "PRINT";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // tbMate
+            // 
+            this.tbMate.Enabled = false;
+            this.tbMate.Location = new System.Drawing.Point(201, 65);
+            this.tbMate.Name = "tbMate";
+            this.tbMate.Size = new System.Drawing.Size(200, 20);
+            this.tbMate.TabIndex = 7;
+            this.tbMate.Text = "SO1922V";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(20, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 24);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Material:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(337, 188);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 16);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "(4 chữ số)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(337, 226);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 16);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "(5 chữ số)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 363);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tbMate);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.tbCavity);
+            this.Controls.Add(this.tbQuantity);
+            this.Controls.Add(this.tbSerial);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.tbMaterial);
+            this.Controls.Add(this.tbSup);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "QC_Label";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbSup;
+        private System.Windows.Forms.TextBox tbMaterial;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.TextBox tbSerial;
+        private System.Windows.Forms.TextBox tbQuantity;
+        private System.Windows.Forms.TextBox tbCavity;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.TextBox tbMate;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ QC_Label/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QC_Label
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dtpDate.Format = DateTimePickerFormat.Custom;
+            dtpDate.CustomFormat = "dd-MM-yyyy";
+        }
+
+        class RawPrinterHelper
+        {
+            // Structure and API declarions:
+            [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+            public class DOCINFOA
+            {
+                [MarshalAs(UnmanagedType.LPStr)] public string pDocName;
+                [MarshalAs(UnmanagedType.LPStr)] public string pOutputFile;
+                [MarshalAs(UnmanagedType.LPStr)] public string pDataType;
+            }
+            [DllImport("winspool.Drv", EntryPoint = "OpenPrinterA", SetLastError = true, CharSet = CharSet.Ansi, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+            public static extern bool OpenPrinter([MarshalAs(UnmanagedType.LPStr)] string szPrinter, out IntPtr hPrinter, IntPtr pd);
+
+            [DllImport("winspool.Drv", EntryPoint = "ClosePrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+            public static extern bool ClosePrinter(IntPtr hPrinter);
+
+            [DllImport("winspool.Drv", EntryPoint = "StartDocPrinterA", SetLastError = true, CharSet = CharSet.Ansi, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+            public static extern bool StartDocPrinter(IntPtr hPrinter, Int32 level, [In, MarshalAs(UnmanagedType.LPStruct)] DOCINFOA di);
+
+            [DllImport("winspool.Drv", EntryPoint = "EndDocPrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+            public static extern bool EndDocPrinter(IntPtr hPrinter);
+
+            [DllImport("winspool.Drv", EntryPoint = "StartPagePrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+            public static extern bool StartPagePrinter(IntPtr hPrinter);
+
+            [DllImport("winspool.Drv", EntryPoint = "EndPagePrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+            public static extern bool EndPagePrinter(IntPtr hPrinter);
+
+            [DllImport("winspool.Drv", EntryPoint = "WritePrinter", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+            public static extern bool WritePrinter(IntPtr hPrinter, IntPtr pBytes, Int32 dwCount, out Int32 dwWritten);
+
+            // SendBytesToPrinter()
+            // When the function is given a printer name and an unmanaged array
+            // of bytes, the function sends those bytes to the print queue.
+            // Returns true on success, false on failure.
+            public static bool SendBytesToPrinter(string szPrinterName, IntPtr pBytes, Int32 dwCount)
+            {
+                Int32 dwError = 0, dwWritten = 0;
+                IntPtr hPrinter = new IntPtr(0);
+                DOCINFOA di = new DOCINFOA();
+                bool bSuccess = false; // Assume failure unless you specifically succeed.
+
+                di.pDocName = "My C#.NET RAW Document";
+                di.pDataType = "RAW";
+
+                // Open the printer.
+                if (OpenPrinter(szPrinterName.Normalize(), out hPrinter, IntPtr.Zero))
+                {
+                    // Start a document.
+                    if (StartDocPrinter(hPrinter, 1, di))
+                    {
+                        // Start a page.
+                        if (StartPagePrinter(hPrinter))
+                        {
+                            // Write your bytes.
+                            bSuccess = WritePrinter(hPrinter, pBytes, dwCount, out dwWritten);
+                            EndPagePrinter(hPrinter);
+                        }
+                        EndDocPrinter(hPrinter);
+                    }
+                    ClosePrinter(hPrinter);
+                }
+                // If you did not succeed, GetLastError may give more information
+                // about why not.
+                if (bSuccess == false)
+                {
+                    dwError = Marshal.GetLastWin32Error();
+                }
+                return bSuccess;
+            }
+
+            public static bool SendFileToPrinter(string szPrinterName, string szFileName)
+            {
+                // Open the file.
+                FileStream fs = new FileStream(szFileName, FileMode.Open);
+                // Create a BinaryReader on the file.
+                BinaryReader br = new BinaryReader(fs);
+                // Dim an array of bytes big enough to hold the file's contents.
+                Byte[] bytes = new Byte[fs.Length];
+                bool bSuccess = false;
+                // Your unmanaged pointer.
+                IntPtr pUnmanagedBytes = new IntPtr(0);
+                int nLength;
+
+                nLength = Convert.ToInt32(fs.Length);
+                // Read the contents of the file into the array.
+                bytes = br.ReadBytes(nLength);
+                // Allocate some unmanaged memory for those bytes.
+                pUnmanagedBytes = Marshal.AllocCoTaskMem(nLength);
+                // Copy the managed byte array into the unmanaged array.
+                Marshal.Copy(bytes, 0, pUnmanagedBytes, nLength);
+                // Send the unmanaged bytes to the printer.
+                bSuccess = SendBytesToPrinter(szPrinterName, pUnmanagedBytes, nLength);
+                // Free the unmanaged memory that you allocated earlier.
+                Marshal.FreeCoTaskMem(pUnmanagedBytes);
+                return bSuccess;
+            }
+            public static bool SendStringToPrinter(string szPrinterName, string szString)
+            {
+                IntPtr pBytes;
+                Int32 dwCount;
+                // How many characters are in the string?
+                dwCount = szString.Length;
+                // Assume that the printer is expecting ANSI text, and then convert
+                // the string to ANSI text.
+                pBytes = Marshal.StringToCoTaskMemAnsi(szString);
+                // Send the converted ANSI string to the printer.
+                SendBytesToPrinter(szPrinterName, pBytes, dwCount);
+                Marshal.FreeCoTaskMem(pBytes);
+                return true;
+            }
+
+            public static bool SendTextFileToPrinter(string szFileName, string printerName)
+            {
+                var sb = new StringBuilder();
+
+                using (var sr = new StreamReader(szFileName, Encoding.Default))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        sb.AppendLine(sr.ReadLine());
+                    }
+                }
+
+                return RawPrinterHelper.SendStringToPrinter(printerName, sb.ToString());
+            }
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            string strQRCode = "";
+            string strDate = dtpDate.Value.ToString("yyMMdd");
+            string strQuantity = tbQuantity.Text.Trim();
+            while (strQuantity.Length < 5)
+            {
+                strQuantity = "0" + strQuantity;
+            }
+            string strCavity=tbCavity.Text.ToUpper().Trim();
+            while (strCavity.Length < 3)
+            {
+                strCavity="0" + strCavity;
+            }
+            strQRCode = tbSup.Text.ToUpper().Trim() + "000" + tbMaterial.Text.ToUpper().Trim() + strDate + tbSerial.Text.ToUpper() + strQuantity + strCavity;
+            string prin = "";
+            prin = "CT~~CD,~CC^~CT~" +
+"^XA" +
+"~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR4,4^MD30^LRN^CI0" +
+"^MMT" +
+"^PW599" +
+"^LL0240" +
+"^LS0" +
+"^FO0,0^GFA,01280,01280,00020,:Z64:" +
+"eJzt0sFKw0AQANAJC82lJNeCa/sL6U1oMB/TH0gJ2EKpHU/evBeaf6gfIBY8eLM/kMKKBy/aLvRgpDFj1t3EiGfx0rksPJiZnWEADvGfEZqXYc1GAIKhYy0hIGks1AaiMlvZlmETlnBiCtmhBH/bRx5QzQTwTYQtz/62nLL2Zo/dRc2IMifvYVfaVQ93sE6c7Aj7t6TMVr90onUCLwxXXkPlahvGifpf7ulS2m6SIN/hg2dhaTDsFbbHZkD3RKRnOxsnlhhjazK7rOw8mluih95khm65l/xtziRbhDVz3585y5gkZZRq2624nQepLKxjzBlecZcIYUrIy6VHbd5+3SN8zNA3c0Do89NiDnetTBobcV+wi7QwqCz11byPsTKhbbBLlVnxNYIlTI+nL2P8GKFjbHAnlTV4kdZZ/riDJrDfN3KIv4tPGOGjig==:E7F3" +
+"^FO0,32^GFA,01280,01280,00020,:Z64:" +
+"eJzt0jFOwzAUBuAXecjmHACTXIEVNWquAuICiSLRoFb0cQG4QLkDK0OlSAxs9AIustSBBdpUHZqqqY1b24WhK2LpWyx9suX/PT2AY/1npYesABAEQR/Bg7PUWHVG1MiQv7WZNlUTSZ1VEM+u0FMNsNCZADbN0SsLYJEyJlUTTtdIygpYYk2phsoW+nf6/nm5syAbc9qcYDARRK1KH7XRfMzhk2C0LImaW+s88m2+Xh8hvnQ25IlcYJEAxJ4x6LS0rRHa2qLGBLzuck90EWqA/ik3dpsPPNHSfUjoD9+NyeWAVOTJ16Y2bybf6oORhlSRoN7m2dpixHyZ1Epbc2OGRTv3LFAKe4ISntkB5iELv9Z4oW0wfzX50pi1t3MWFNjeChY7s71BWsfgzPWWLeqd6Xza7B+TH/OtZS/V/q2zX3vADqzEsf6ovgHKMada:7317" +
+"^FO0,64^GFA,01280,01280,00020,:Z64:" +
+"eJzt0TFKxUAQBuA/bJFGkvYJ4h7CJmB8exXBCwRsAoaXBcHWCwhewdJCMPCKXGPEIp1ZsIk8yTgxJlkvoM0b2JD9mGFnZ4F9/Hskv7epLEqgLMGwm1MOxK7uSH6z0XJJFTt98qxkOmZ25tGr5dFOHrw8AkVSy8zVZAENOtpUqyqQEgtf6mEb2uHDjmKx+L1WkvttunRkxPRusQgZpS1T+bzUQgxinW68C4u9WlzctkvPgymL8KZFkvz0LOcauQf3b97dJuuXPCoJukKUbDyLCWdbRHmx1JKsj8bqLB3mO/ZHWCnHMFT4dqRdgcOul/F2s5lsDbVrrWdpesk2vq+r+UmZPwNu3MZcb2Uu0xs51QcuXweLjfM/xwr7+Mv4AjOFsf8=:217B" +
+"^FT489,160^A0N,102,100^FB98,1,0^FH\\^FD01^FS" +
+"^FT169,44^A0N,28,28^FB168,1,0^FH\\^FD"+tbMaterial.Text.ToUpper().Trim()+"^FS" +
+"^FT169,76^A0N,28,28^FB103,1,0^FH\\^FD"+tbMate.Text.ToUpper().Trim()+"^FS" +
+"^FT169,108^A0N,28,28^FB42,1,0^FH\\^FD"+tbQuantity.Text+"^FS" +
+"^FT169,155^A0N,28,26^FB278,1,0^FH\\^FD"+ tbSup.Text.ToUpper().Trim() + "000" + tbMaterial.Text.ToUpper().Trim() + "^FS" +
+"^FT31,242^BQN,2,4" +
+"^FH\\^FDLA,"+strQRCode+"^FS" +
+"^FT169,189^A0N,28,26^FB236,1,0^FH\\^FD"+ strDate + tbSerial.Text.ToUpper() + strQuantity + strCavity + "^FS" +
+"^XZ";
+            PrintDialog pd = new PrintDialog();
+            pd.Document = new PrintDocument();
+            pd.PrinterSettings = new PrinterSettings();
+
+            RawPrinterHelper.SendStringToPrinter(pd.PrinterSettings.PrinterName, prin);
+        }
+    }
+}
